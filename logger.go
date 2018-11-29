f1c1262b869fafdcb110af51477f2a0f05dc84d9
Johan Sundell <johan@pixpro.net>
2018-11-29 09:36:53 +0100

    Fixed formating

--- logger.go
+++ logger.go
@@ -13,9 +13,11 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Println(err)
 	}
-	for k, v := range r.Form {
+	/*for k, v := range r.Form {
 		fmt.Println(k, v)
-	}
+	}*/
+	s := fmt.Sprintf("%+v", r.Form)
+	fmt.Println(s)
 	//fmt.Println("done")
 
 	fmt.Fprintf(w, "Oki ;)")
